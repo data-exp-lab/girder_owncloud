4faa13b6777a1d73041662515fdc643dee939caa
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2016-09-27 14:18:55 -0500

    Add endpoint to obtain OC creds

--- server/__init__.py
+++ server/__init__.py
@@ -5,6 +5,7 @@ import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.fernet import Fernet
 import json
 import os
 import random
@@ -15,10 +16,10 @@ import string
 from girder import events
 from girder.api import access
 from girder.api.describe import Description, describeRoute
-from girder.api.rest import loadmodel
+from girder.api.rest import loadmodel, getCurrentToken
 from girder.models.model_base import AccessType, ValidationException
-from girder.plugins.ythub.constants import \
-    PluginSettings as ythubPluginSettings
+# from girder.plugins.ythub.constants import \
+#    PluginSettings as ythubPluginSettings
 from girder.utility import setting_utilities
 from girder.utility.model_importer import ModelImporter
 from .constants import PluginSettings
@@ -73,7 +74,7 @@ def updateOwnCloudPassword(user, params):
     )
 
     private_key = serialization.load_pem_private_key(
-        settings.get(ythubPluginSettings.HUB_PRIV_KEY).encode('utf8'),
+        settings.get('ythub.priv_key').encode('utf8'),
         password=None,
         backend=default_backend()
     )
@@ -99,8 +100,27 @@ def updateOwnCloudPassword(user, params):
     payload = {'message': base64.b64encode(ciphertext).decode('utf8'),
                'signature': base64.b64encode(signature).decode('utf8')}
     r = requests.post(oc_url + '/user', data=json.dumps(payload))
-    # print(r.status_code)
     return
+
+
+@access.user
+@loadmodel(model='user', level=AccessType.WRITE)
+@describeRoute(
+    Description('Get OwnCloud credentials.')
+    .param('id', 'The ID of the user', paramType='path')
+)
+def getOwnCloudPassword(user, params):
+    if not user.get('ocpass'):
+        user = ModelImporter.model('user').save(user)
+
+    credentials = json.dumps(
+        {'user': user['login'], 'pass': user['ocpass']}
+    ).encode('utf8')
+
+    token = getCurrentToken()
+    key = token['_id'][:32]
+    credentials = Fernet(base64.b64encode(key)).encrypt(credentials)
+    return {'credentials': credentials.decode('utf8')}
 
 
 def _userUpdate(event):
@@ -115,6 +135,8 @@ def _userUpdate(event):
 def load(info):
     info['apiRoot'].user.route(
         'PUT', (':id', 'ocpass'), updateOwnCloudPassword)
+    info['apiRoot'].user.route(
+        'GET', (':id', 'ocpass'), getOwnCloudPassword)
     events.bind('model.user.save', 'owncloud', _userUpdate)
     # ModelImporter.model('user').exposeFields(
     #    level=AccessType.WRITE, fields='ocpass')
