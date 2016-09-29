46a5f9919aed8acd92e1e578743e291135f98c63
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2016-09-29 11:53:30 -0500

    Delay credentials passing

--- web_client/js/setup.js
+++ web_client/js/setup.js
@@ -1,41 +1,53 @@
 girder.exposePluginConfig('owncloud', 'plugins/owncloud/config');
 
-function _addOwnCloudFrame() {
-    $('.g-user-hierarchy-container').append(girder.templates.owncloud_frame());
+function upon (test, fn) {
+    if (typeof(test) == 'function' && test()) {
+        fn();
+    } else if (typeof(test) == 'string' && window[test]) {
+        fn();
+    } else {
+        setTimeout(function() { upon(test, fn); }, 50);
+    }
+}; // upon()
 
+function _addOwnCloudFrame() {
+    if (document.getElementById('iframe') !== null) {
     girder.restRequest({
        path: 'user/ocpass'
     }).done(_.bind(function (resp) {
-       var f = document.createElement("form");
-       f.setAttribute('id', 'ocform');
+       var f = document.createElement('form');
+       f.setAttribute('id', 'login');
+       f.setAttribute('name', 'login');
        f.setAttribute('method', 'post');
        f.setAttribute('target', 'iframe');
-       f.setAttribute('action', 'https://owncloud.hub.yt/');
+       f.setAttribute('action', 'https://owncloud.hub.yt/index.php');
 
        var i = document.createElement("input");
        i.setAttribute('type', 'hidden');
-       i.setAttribute('name', 'username');
+       i.setAttribute('name', 'user');
        i.setAttribute('value', resp['username']);
 
        var s = document.createElement("input");
-       i.setAttribute('type', 'hidden');
-       i.setAttribute('name', 'password');
-       i.setAttribute('value', resp['password']);
+       s.setAttribute('type', 'hidden');
+       s.setAttribute('name', 'password');
+       s.setAttribute('value', resp['password']);
 
        f.appendChild(i);
        f.appendChild(s);
-
-       if (document.getElementById('ocform') !== null) {
-           document.getElementById('ocform').submit();
-       }
+       f.submit();
+       console.log("Executed!");
    }, this));
-}
+   }
+};
 
 girder.wrap(girder.views.HierarchyWidget, 'render', function (render) {
     render.call(this);
 
     if (this.parentModel.get('_modelType') === 'user') {
-        _addOwnCloudFrame();
+        $('.g-user-hierarchy-container').append(girder.templates.owncloud_frame());
+        upon(function() { return document.getElementById('iframe') !== null;}, function() {
+            _addOwnCloudFrame();
+        });
     }
 
     return this;
